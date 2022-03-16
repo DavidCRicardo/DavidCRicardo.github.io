848cd6a8494d638db896f821f857b1836d059d63
David Ricardo <david.c.ricardo@gmail.com>
2022-03-16 15:30:21 +0000

    Small changes related to Projects Organization on page

--- scripts/core.js
+++ scripts/core.js
@@ -42,110 +42,107 @@ class PortfolioProject {
 };
 
 var mainProjects = [
-        new PortfolioProject(
-            "Friendly Lords",
-            "https://www.nexusmods.com/mountandblade2bannerlord/mods/3230",
-            "img/projects/FriendlyLords.jpeg",
-            "img/projects/FriendlyLords2.jpeg",
-            `Developed using C# with Visual Studio. 
-        Bannerlord Mod.`,
-            "Academic Project"
-        ),
-
-        new PortfolioProject(
-            "Kan Samurai Game",
-            "https://david-ricardo.itch.io/kansamurai",
-            "img/projects/devlog.png",
-            "img/projects/devlog2.png",
-            `Developed using C++ and Blueprints with Unreal Engine.
+    new PortfolioProject(
+        "Friendly Lords",
+        "https://www.nexusmods.com/mountandblade2bannerlord/mods/3230",
+        "img/projects/FriendlyLords.jpeg",
+        "img/projects/FriendlyLords2.jpeg",
+        `Developed using C# with Visual Studio. 
+Bannerlord Mod.`,
+        "Academic Project (2021)"
+    ),
+    new PortfolioProject(
+        "Asteroids_Clone",
+        "https://david-ricardo.itch.io/asteroids-clone",
+        "img/projects/Asteroids_Clone.png",
+        "img/projects/Asteroids_Clone_2.png",
+        `Developed using C++ with Unreal Engine.
+         I'm the only developer.`,
+        "Personal Project (2019)"
+    ),
+    new PortfolioProject(
+        "Two-Factor Authentication",
+        "https://github.com/DavidCRicardo/TwoFactor-Authentication",
+        "img/projects/PG1.png",
+        "img/projects/PG2.png",
+        `Developed using C# and Xamarin on Visual Studio.
         I'm the only developer.`,
-            "Personal Project"
-        ),
+        "Academic Project (2019)"
+    )
+],
 
-        new PortfolioProject(
-            "Asteroids_Clone",
-            "https://david-ricardo.itch.io/asteroids-clone",
-            "img/projects/Asteroids_Clone.png",
-            "img/projects/Asteroids_Clone_2.png",
-            `Developed using C++ with Unreal Engine.
+releasedProjects = [       
+    
+],
+
+otherProjects = [
+    new PortfolioProject(
+        "Kan Samurai Demo",
+        "https://david-ricardo.itch.io/kansamurai",
+        "img/projects/devlog.png",
+        "img/projects/devlog2.png",
+        `Developed using C++ and Blueprints with Unreal Engine.
         I'm the only developer.`,
-            "Personal Project"
-        ),
-
-        new PortfolioProject(
-            "TalesOfShadows",
-            "https://david-ricardo.itch.io/tales-of-shadows",
-            "img/projects/TalesOfShadows.png",
-            "img/projects/TalesOfShadows_2.png",
-            `Developed using C++ and Blueprints with Unreal Engine.
+        "Personal Project (2021)"
+    ),
+    new PortfolioProject(
+        "TalesOfShadows",
+        "https://david-ricardo.itch.io/tales-of-shadows",
+        "img/projects/TalesOfShadows.png",
+        "img/projects/TalesOfShadows_2.png",
+        `Developed using C++ and Blueprints with Unreal Engine.
             I'm the only developer.`,
-            "Personal Project"
-        ),
-
-        new PortfolioProject(
-            "EscapeLab",
-            "https://david-ricardo.itch.io/escapelab",
-            "img/projects/escapelab_gameplay.png",
-            "img/projects/escapeLab_Menu.png",
-            `Developed using C# with Unity.
-            I'm the only developer.`,
-            "Personal Project"
-        )
-    ],
-
-    releasedProjects = [
-        new PortfolioProject(
-            "RPG 2D",
-            "https://youtu.be/TdcC63ItCzk",
-            "img/projects/rpg_2d_demo.png",
-            "img/projects/rpg2D_Menu2.png",
-            `Developed using HTML, CSS and Javascript.
+        "Personal Project (2019)"
+    ),
+    new PortfolioProject(
+        "Launcher Prototype",
+        "https://github.com/DavidCRicardo/Launcher_prototype",
+        "img/projects/launcher-demo1.png",
+        "img/projects/launcher-demo2.png",
+        `Developed using C# on Visual Studio.
         I'm the only developer.`,
-            "Personal Project"
-        ),
-
-        new PortfolioProject(
-            "BreakOut",
-            "https://youtu.be/W5P5F5Ryh-8",
-            "img/projects/breakout_1.png",
-            "img/projects/BreakOut_Menu.png",
-            `Developed using C# on Visual Studio.
+        "Personal Project (2019)"
+    ),
+    new PortfolioProject(
+        "EscapeLab",
+        "https://david-ricardo.itch.io/escapelab",
+        "img/projects/escapelab_gameplay.png",
+        "img/projects/escapeLab_Menu.png",
+        `Developed using C# with Unity.
+    I'm the only developer.`,
+        "Personal Project (2017)"
+    ),
+   
+    new PortfolioProject(
+        "RPG 2D",
+        "https://youtu.be/TdcC63ItCzk",
+        "img/projects/rpg_2d_demo.png",
+        "img/projects/rpg2D_Menu2.png",
+        `Developed using HTML, CSS and Javascript.
         I'm the only developer.`,
-            "Academic Project"
-        ),
+        "Personal Project (2017)"
+    ),
 
-        new PortfolioProject(
-            "PingPong",
-            "https://youtu.be/iodkJTezckY",
-            "img/projects/pingpong_1.png",
-            "img/projects/Ping_Pong_Menu.png",
-            `Developed using C# on Visual Studio.
+    new PortfolioProject(
+        "BreakOut",
+        "https://youtu.be/W5P5F5Ryh-8",
+        "img/projects/breakout_1.png",
+        "img/projects/BreakOut_Menu.png",
+        `Developed using C# on Visual Studio.
         I'm the only developer.`,
-            "Personal Project"
-        )
-    ],
+        "Academic Project (2017)"
+    ),
 
-    var otherProjects = [
-        new PortfolioProject(
-            "Two-Factor Authentication",
-            "https://github.com/DavidCRicardo/TwoFactor-Authentication",
-            "img/projects/PG1.png",
-            "img/projects/PG2.png",
-            `Developed using C# and Xamarin on Visual Studio.
-            I'm the only developer.`,
-            "Academic Project"
-        ),
-
-        new PortfolioProject(
-            "Launcher Prototype",
-            "https://github.com/DavidCRicardo/Launcher_prototype",
-            "img/projects/launcher-demo1.png",
-            "img/projects/launcher-demo2.png",
-            `Developed using C# on Visual Studio.
+    new PortfolioProject(
+        "PingPong",
+        "https://youtu.be/iodkJTezckY",
+        "img/projects/pingpong_1.png",
+        "img/projects/Ping_Pong_Menu.png",
+        `Developed using C# on Visual Studio.
         I'm the only developer.`,
-            "Personal Project"
-        )
-    ];
+        "Personal Project (2017)"
+    )
+];
 
 
 function insertMainProjects() {
